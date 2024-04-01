868b610b288e000be123fd6adbf9412c05bc56d2
augusta-rabe <146953117+augusta-rabe@users.noreply.github.com>
2024-04-01 14:38:35 +0300

    Création de la classe MoneyTest

---
+++ GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyTest.java
@@ -0,0 +1,14 @@
+package fr.emse.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MoneyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
