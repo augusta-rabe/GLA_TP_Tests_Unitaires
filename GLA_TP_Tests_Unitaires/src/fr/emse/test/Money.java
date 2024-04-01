63fe5ad5823dea20a530a6cb19511f69114c6bd0
augusta-rabe <146953117+augusta-rabe@users.noreply.github.com>
2024-04-01 17:30:01 +0300

    Surcharge de la méthode equals de la classe Money

--- GLA_TP_Tests_Unitaires/src/fr/emse/test/Money.java
+++ GLA_TP_Tests_Unitaires/src/fr/emse/test/Money.java
@@ -1,24 +1,35 @@
 package fr.emse.test;
 
 class Money {
-	private int fAmount;
-	private String fCurrency;
+    private int fAmount;
+    private String fCurrency;
 
-	public Money(int amount, String currency) {
-		fAmount = amount;
-		fCurrency = currency;
-	}
-	
-	public int amount() {
-		return fAmount;
-	}
-	
-	public String currency() {
-		return fCurrency;
-	}
-	
-	public Money add(Money m) {
-		return new Money(amount() + m.amount(), currency());
-	}
-	
+    public Money(int amount, String currency) {
+        fAmount = amount;
+        fCurrency = currency;
+    }
+
+    public int amount() {
+        return fAmount;
+    }
+
+    public String currency() {
+        return fCurrency;
+    }
+
+    public Money add(Money m) {
+        return new Money(amount() + m.amount(), currency());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Money money = (Money) obj;
+        return fAmount == money.fAmount && fCurrency.equals(money.fCurrency);
+    }
 }
