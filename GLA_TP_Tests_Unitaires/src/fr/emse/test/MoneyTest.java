3bbb716363348b488861fb6221c2c51e8a5d954f
augusta-rabe <146953117+augusta-rabe@users.noreply.github.com>
2024-04-01 16:33:56 +0300

    Ajout d'une méthode de test d'égalité

--- GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyTest.java
+++ GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyTest.java
@@ -10,6 +10,18 @@ public class MoneyTest {
         Money m14CHF = new Money(14, "CHF");
         Money expected = new Money(26, "CHF");
         Money result = m12CHF.add(m14CHF);
+        
         assertTrue(expected.equals(result));
+    }
+    
+    @Test
+    public void testEquals() {
+	    Money m12CHF= new Money(12, "CHF");
+	    Money m14CHF= new Money(14, "CHF");
+	    
+	    assertTrue(!m12CHF.equals(null));
+	    assertEquals(m12CHF, m12CHF);
+	    assertEquals(m12CHF, new Money(12, "CHF"));
+	    assertTrue(!m12CHF.equals(m14CHF));
     }
 }
