d3b874b8c4a244922a9f53ec7063d35b92f07e3f
augusta-rabe <146953117+augusta-rabe@users.noreply.github.com>
2024-04-01 19:03:05 +0300

    Jeu de test pour vérifier la simplification des MoneyBag en Money

--- GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyBagTest.java
+++ GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyBagTest.java
@@ -31,4 +31,27 @@ public class MoneyBagTest {
         assertTrue(!f12CHF.equals(fMB1));
         assertTrue(!fMB1.equals(fMB2));
     }
+    
+    @Test
+    public void testSimplifyMoneyBag() {
+        Money m7USD = new Money(7, "USD");
+        Money m12CHF = new Money(12, "CHF");
+        MoneyBag mb1 = new MoneyBag(m7USD, m12CHF);
+
+        Money mMinus12CHF = new Money(-12, "CHF");
+
+        // Ajout d'un Money à un MoneyBag
+        IMoney result1 = mb1.add(mMinus12CHF);
+        assertTrue(result1 instanceof Money);
+        assertEquals(new Money(7, "USD"), result1);
+
+        // Ajout d'un MoneyBag à un MoneyBag
+        MoneyBag mb2 = new MoneyBag(new Money(5, "USD"), new Money(10, "CHF"));
+        IMoney result2 = mb1.add(mb2);
+        assertTrue(result2 instanceof MoneyBag);
+        assertEquals(new Money(12, "CHF"), ((MoneyBag) result2).getMonies().get(0));
+        assertEquals(new Money(12, "USD"), ((MoneyBag) result2).getMonies().get(1));
+    }
+
+    
 }
