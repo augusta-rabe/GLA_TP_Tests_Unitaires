95803b81320ca7b25de4f7b50814b97bc2ac3ecc
augusta-rabe <146953117+augusta-rabe@users.noreply.github.com>
2024-04-01 16:02:59 +0300

    Ajout d'une méthode de test dans MoneyTest

--- GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyTest.java
+++ GLA_TP_Tests_Unitaires/src/fr/emse/test/MoneyTest.java
@@ -1,14 +1,15 @@
 package fr.emse.test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class MoneyTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+public class MoneyTest {
+    @Test
+    public void testSimpleAdd() {
+        Money m12CHF = new Money(12, "CHF");
+        Money m14CHF = new Money(14, "CHF");
+        Money expected = new Money(26, "CHF");
+        Money result = m12CHF.add(m14CHF);
+        assertTrue(expected.equals(result));
+    }
 }
